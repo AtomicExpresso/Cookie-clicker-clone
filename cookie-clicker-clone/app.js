c74a754f626775b25c6187f049c4834ba46e480b
AtomicExpresso <156177896+AtomicExpresso@users.noreply.github.com>
2024-03-01 14:58:56 -0500

    Update app.js
    Added a class for the shop and instances for the items, Also fixed the quality issue that would make the shop and item screen overlap

--- cookie-clicker-clone/app.js
+++ cookie-clicker-clone/app.js
@@ -22,14 +22,15 @@ let cookiePerSec = 0; //Tracks how many clicks per second you get
 
 //Shop Variables
 let autoClick = 0;
+
+//Screen variables (Used to prevent overlapping and allow screens to be opened/closed)
+let itemBtnClick = 0;
 let shopBtnClick = 0;
 
-//Inventory Screen variables
-let itemBtnClick = 0;
+//Shop class instances (For each shop class instance, sets the items defualt value to undefined until declared)
+let autoClickItem = undefined;
 
-//Settings
-
-//==============Logic===========================
+//==============Cookie click Logic===========================
 
 //Main cookie function
 const mainCookieClick = () => {
@@ -53,17 +54,57 @@ document.getElementById('bgani').ondragstart = function () {
   return false;
 };
 
-//Shop
-const autoClickShop = () => {
-  if (cookieClick >= 100) {
-    //tests if you have enough to buy the item
-    cookieClick -= 100;
-    autoClick += 1;
-    cookieCountText.innerText = `${cookieClick} Cookies`; //changes your balence
+//================Shop Logic==============================
 
-    shopinfotext.innerText =
-      'You bought a click upgrade! Your clicks are increased by +1';
+//This is a class definition that represents a shop item
+//Item price = The items price
+//ItemName = is the name of the item
+//Item = is the value of the item
+
+class shop {
+  constructor(itemName, price, item) {
+    //The constructor initializes these properties when a new shop item is created
+    this._itemName = itemName;
+    this._price = price;
+    this._item = item;
+  }
+
+  get price() {
+    //retrives the price of the shop item
+    return this._price;
+  }
+
+  get itemName() {
+    return this._itemName;
+  }
+}
+
+//Evaluate the price, it takes 'Shop instance' as a paramenter, which represents the instance of the shop class
+
+let evaluatePrice = (shopInstance) => {
+  //Shop instance catchs the item
+  if (cookieClick >= shopInstance.price) {
+    //if cookies is greater then or equal to the price of shopInstance
+    cookieClick -= shopInstance.price;
+
+    shopinfotext.innerText = `You bought a "${shopInstance.itemName}"`;
     shopinfotext.style.color = 'green';
+    cookieCountText.innerText = `${cookieClick} cookies`;
+  } else {
+    //else say you cant afford it
+    shopinfotext.innerText = 'You cannot afford to buy this!';
+    shopinfotext.style.color = 'red';
+  }
+};
+
+//Auto Click Item, creates a new instance of a shop item (in this case, it creates an instance named autoClickItem)
+const autoClickShop = () => {
+  autoClickItem = new shop('Click Upgrade', 10);
+
+  // Evaluate the price and deduct from cookie count only if the player can afford it
+  if (cookieClick >= autoClickItem.price) {
+    evaluatePrice(autoClickItem); //passes the shopClickItem to the shop
+    autoClick++;
   } else {
     shopinfotext.innerText = 'You cannot afford to buy this!';
     shopinfotext.style.color = 'red';
@@ -73,18 +114,26 @@ const autoClickShop = () => {
 const shopBtnFn = () => {
   shopScreen.classList.add('shop-screen');
   shopinfotext.style.color = 'black';
-  shopinfotext.innerText = 'Buy upgrades and stuff here'
+  shopinfotext.innerText = 'Buy upgrades and stuff here';
   shopBtnClick++;
 
   if (shopBtnClick >= 2) {
     shopBtnClick = 0;
     shopScreen.classList.remove('shop-screen');
   }
+
+  
+  if (itemBtnClick >= 1) { //prevents the screens from overlapping
+    itemBtnClick = 0;
+    itemScreen.classList.remove('item-screen');
+  }
 };
 
 const closeShopFn = () => {
   shopScreen.classList.remove('shop-screen');
 };
+
+//===========Items logic===============
 
 //Item screen
 const itemBtnFn = () => {
@@ -98,11 +147,17 @@ const itemBtnFn = () => {
     itemBtnClick = 0;
     itemScreen.classList.remove('item-screen');
   }
+
+  if (shopBtnClick >= 1) { //prevents the screens from overlapping
+    shopBtnClick = 0;
+    shopScreen.classList.remove('shop-screen');
+  }
 };
 
 const closeItemFn = () => {
   itemScreen.classList.remove('item-screen');
 };
+
 //==========================
 //Click's Per second effects
 //==========================
@@ -110,7 +165,7 @@ const closeItemFn = () => {
 setInterval(function cookiesec() {
   cookiePerSecText.innerText = `Clicks ps: ${cookiePerSec}`;
   cookiePerSec = 0;
-}, 1500);
+}, 1000);
 
 mainCookie.addEventListener('click', function () {
   //Plays the RGB animation if the player's clicks per second goes over 8
